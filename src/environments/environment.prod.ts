66f128303c85cbbab2615610c9c7a41f8d4d7b7f
R-Dev-09 <52428932+R-Dev-09@users.noreply.github.com>
2021-07-23 14:50:19 +0200

    modded env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,6 @@ export const environment = {
       'stun:stun2.l.google.com:19302',
     ]}
   ],
-  // socketUrl: 'https://rd-ng-sockets.herokuapp.com',
   socketUrl: 'http://localhost:3000',
-  socketOptions: {path: '/collaborate', auth: {token: 'Naris', pw: 'Naris'}}
+  socketOptions: {}
 };
